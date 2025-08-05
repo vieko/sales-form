9f71db9fdc573d866472536df789d1af60b46a7e
Vieko Franetovic <48670+vieko@users.noreply.github.com>
2025-08-05 14:37:53 -0600

    adds vercel theme, adds theme provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import { ThemeProvider } from '@/components/theme-provider'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 
@@ -23,11 +24,18 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem={true}
+          disableTransitionOnChange={true}
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   )
