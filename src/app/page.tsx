d2d4c19426345c3262b7342af3a1eb02a2c811c6
Vieko Franetovic <48670+vieko@users.noreply.github.com>
2025-08-06 17:06:44 -0600

    adds form elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import { SalesForm } from '@/components/sales-form'
 export default function Home() {
   return (
     <div className="flex flex-1 flex-col gap-4 p-4">
       <div className="grid auto-rows-min gap-4 md:grid-cols-2">
-        <div className="bg-muted/50 aspect-video rounded-xl p-4">FORM</div>
-        <div className="bg-muted/50 aspect-video rounded-xl p-4">LOGS</div>
+        <div className="bg-muted/50 rounded-xl p-4">
+          <SalesForm />
+        </div>
+        <div className="bg-muted/50 rounded-xl p-4">LOGS</div>
       </div>
     </div>
   )
--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -5,13 +5,17 @@ export function SiteHeader() {
   return (
     <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
-        <div className="-ml-1">logo</div>
+        <div className="-ml-1">
+          <svg className="h-4 w-4" viewBox="0 0 76 65" fill="currentColor">
+            <path d="m37.59.25 36.95 64H.64l36.95-64z" />
+          </svg>
+        </div>
         <Separator
           orientation="vertical"
           className="mx-2 data-[orientation=vertical]:h-4"
         />
         <h1 className="text-base font-medium">sales form</h1>
-        <div className="ml-auto flex items-center">
+        <div className="-mr-2 ml-auto flex items-center">
           <ModeToggle />
         </div>
       </div>
