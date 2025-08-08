08006dadba1087bf4572ac431d40383e738cfc54
Vieko Franetovic <48670+vieko@users.noreply.github.com>
2025-08-07 18:37:14 -0600

    moved utilities to utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,3 +9,23 @@ export function isValidEmail(email: string) {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
   return emailRegex.test(email)
 }
+
+export const extractDomain = (urlOrEmail: string): string => {
+  try {
+    if (urlOrEmail.includes('@')) {
+      return urlOrEmail.split('@')[1]
+    }
+    const url = new URL(
+      urlOrEmail.startsWith('http') ? urlOrEmail : `https://${urlOrEmail}`,
+    )
+    return url.hostname.replace(/^www\./, '')
+  } catch {
+    return urlOrEmail
+  }
+}
+
+export const extractCompanyName = (email: string, domain: string): string => {
+  const domainParts = domain.split('.')
+  const companyPart = domainParts[0]
+  return companyPart.charAt(0).toUpperCase() + companyPart.slice(1)
+}
